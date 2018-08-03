ef775b93620f9bc2192ddb2620a996b23aaa229d
swh <shi_wenhu@qq.com>
2018-08-03 14:39:40 +0800

    init

---
+++ WPHP/src/Utility/Util.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/3
+ * Time: 14:29
+ */
+
+namespace WPHP\Utility;
+/**
+ * 测试工具类
+ * Class Util
+ * @package WPHP\Utility
+ */
+class Util
+{
+    public static function test()
+    {
+        echo "util...";
+    }
+}
